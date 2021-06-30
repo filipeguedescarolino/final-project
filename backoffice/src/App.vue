f9a775f9550712a558fc65a251d716db607c1323
Filipe <filipe.guedes.c@gmail.com>
2021-06-30 12:44:57 +0100

    identation

--- backoffice/src/App.vue
+++ backoffice/src/App.vue
@@ -1,147 +1,114 @@
 <template>
   <!-- App.vue -->
 
-<v-app>
-  <v-navigation-drawer app permanent>
+    <v-app>
+        <v-navigation-drawer app permanent>
+
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title class="text-h6">
+                        Doctor
+                    </v-list-item-title>
+                    <v-list-item-subtitle>
+                        application
+                    </v-list-item-subtitle>
+                </v-list-item-content>
+            </v-list-item>
+
+            <v-divider></v-divider>
+
+            <v-list
+                dense
+                nav>
+            
+                <v-list-item
+                    :to="item.to"
+                    v-for="item in items"
+                    :key="item.title"
+                    link>
+                
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+
+        </v-navigation-drawer>
+
     
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="text-h6">
-            Doctor
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            application
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+        <v-main>
 
-      <v-divider></v-divider>
+        <!-- Provides the application the proper gutter -->
+            <v-container fluid>
 
-      <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          :to="item.to"
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+        <!-- If using vue-router -->
+                <router-view></router-view>
+            </v-container>
+        </v-main>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    
-  </v-navigation-drawer>
+        <v-footer app
+            color="primary lighten-1"
+            padless
+            center>
+        
+            <v-row
+                justify="center"
+                no-gutters>
+        
 
-  <!-- <v-app-bar app>
-    <v-app-bar
-      absolute
-      color="white"
-      elevate-on-scroll
-      scroll-target="#scrolling-techniques-7"
-    >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+                <v-col
+                    class="primary lighten-2 py-4 text-center white--text"
+                    cols="12">
+                
+                    {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+                </v-col>
+            </v-row>
 
-      <v-toolbar-title>Title</v-toolbar-title>
-
-      <v-spacer></v-spacer>
-
-      <v-btn icon>
-        <h1> asdsa</h1>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>asd</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-  </v-app-bar> -->
-
-  <!-- Sizes your content based upon application components -->
-  <v-main>
-
-    <!-- Provides the application the proper gutter -->
-    <v-container fluid>
-
-      <!-- If using vue-router -->
-      <router-view></router-view>
-    </v-container>
-  </v-main>
-
-  <v-footer app
-   
-    color="primary lighten-1"
-    padless
-    center
-  >
-    <v-row
-      justify="center"
-      no-gutters
-    >
-      
-      <v-col
-        class="primary lighten-2 py-4 text-center white--text"
-        cols="12"
-      >
-        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
-      </v-col>
-    </v-row>
-  
-  </v-footer>
-</v-app>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        drawer: true,
-        items: [
-          { title: 'Create Doctor', to: '/create_doctor' },
-          { title: 'Working period', to: 'working-hours' },
-          { title: 'Specialties', to: 'specialties' },
-          { title: 'Prices', to: 'prices' },
-          { title: 'Income Doctors', to: 'incomedoctor' },
-          { title: 'Income Patients', to: 'incomepatients' },
-          
-        ],
-        
-        
-      
-      }
-    },
-  }
+    export default {
+        data () {
+            return {
+                drawer: true,
+                items: [
+                { title: 'Create Doctor', to: '/create_doctor' },
+                { title: 'Working period', to: 'working-hours' },
+                { title: 'Specialties', to: 'specialties' },
+                { title: 'Prices', to: 'prices' },
+                { title: 'Income Doctors', to: 'incomedoctor' },
+                { title: 'Income Patients', to: 'incomepatients' },
+                ],
+            }
+        },
+    }   
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    }
 
-#nav {
-  padding: 30px;
-}
+    #nav {
+    padding: 30px;
+    }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+    }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+    #nav a.router-link-exact-active {
+    color: #42b983;
+    }
 </style>
--- front/src/App.vue
+++ front/src/App.vue
@@ -1,184 +1,171 @@
 <template>
-  <v-app id="inspire">
-    
-      <v-navigation-drawer
-      v-model="drawer"
-      app
-      fixed
-      dark
-      color="#99D3DF"
-      
-    >
+    <v-app id="inspire">
 
-      <v-list-item class="px-2"
-        v-if="!localStorageToken && !localStorageUser">
-        <v-list-item-avatar>
-         <v-icon>fas fa-lock</v-icon>
-        </v-list-item-avatar>
+        <v-navigation-drawer
+            v-model="drawer"
+            app
+            fixed
+            dark
+            color="#99D3DF">
 
-        <v-list-item-content>
-          <v-list-item class="text-h6 grey--text  text--lighten-5"
-            :to="'/login'"
-            @click="drawer = !drawer">
-            Login 
-          </v-list-item>
+            <v-list-item class="px-2"
+                v-if="!localStorageToken && !localStorageUser">
 
-          <!-- <v-list-item-subtitle>
-            My BooketList
-          </v-list-item-subtitle> -->
-        </v-list-item-content>
-      </v-list-item>
-      <v-list-item v-else>
-        <v-list-item-avatar>
-            <v-img :src="patient.image"> </v-img>
+                <v-list-item-avatar>
+                    <v-icon>fas fa-lock</v-icon>
+                </v-list-item-avatar>
+
+                <v-list-item-content>
+                    <v-list-item class="text-h6 grey--text  text--lighten-5"
+                        :to="'/login'"
+                        @click="drawer = !drawer">
+                        Login 
+                    </v-list-item>
+                </v-list-item-content>
+            </v-list-item>
             
-        </v-list-item-avatar>
-          <h1> {{patient.name}} </h1>
-          
-      </v-list-item>
-        <!-- <button @click="store"> connect to $store.state.counter </button>
-        <h1> {{$store.state.counter}} </h1> -->
-      <v-divider></v-divider>
+            <v-list-item v-else>
+                <v-list-item-avatar>
+                    <v-img :src="patient.image"> </v-img>
+                </v-list-item-avatar>
 
+                <h1> {{patient.name}} </h1>
 
-      <v-list
-        v-if="localStorageToken"
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-
-          
-         
-        </v-list-item>
-
-        <v-list-item link @click="Logout()">
-          <v-list-item-icon>
-            <v-icon>fas fa-home</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title> Logout </v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app
-    color="#88BBD6">
-      <v-app-bar-nav-icon @click="drawer = !drawer" class=" white--text text--darken-2"> </v-app-bar-nav-icon>
-
-      <v-toolbar-title   >
+            </v-list-item>
         
-        <v-list-item   :to="'/about'"> 
-          <span class=" white--text text--darken-2">  About us </span>
-        </v-list-item>
-        
-      </v-toolbar-title>
-      
-      <v-toolbar-title   >
-        <v-list-item  :to="'/profile'"> 
-          <span class=" white--text text--darken-2 border-bottom">  Our specialists </span> 
-        </v-list-item>
-      </v-toolbar-title>
+            <v-divider></v-divider>
 
-      <v-toolbar-title   >
-        <v-list-item  :to="'/partnerships'"> 
-          <span class=" white--text text--darken-2">  Partnerships </span>
-        </v-list-item>
-      </v-toolbar-title>
 
-      <v-toolbar-title   >
-        <v-list-item  :to="'/prices'"> 
-          <span class=" white--text text--darken-2">  Prices </span>
-        </v-list-item>
-      </v-toolbar-title>
-    </v-app-bar>
+            <v-list
+                v-if="localStorageToken"
+                dense
+                nav>
+                
+                <v-list-item
+                    v-for="item in items"
+                    :key="item.title"
+                    :to="item.to"
+                    link>
+            
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
 
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-  </v-app>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+
+
+
+                </v-list-item>
+
+                <v-list-item link @click="Logout()">
+                    <v-list-item-icon>
+                        <v-icon>fas fa-home</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title> Logout </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app
+            color="#88BBD6">
+
+            <v-app-bar-nav-icon @click="drawer = !drawer" class=" white--text text--darken-2"> </v-app-bar-nav-icon>
+
+            <v-toolbar-title>
+                <v-list-item   :to="'/about'"> 
+                    <span class=" white--text text--darken-2">  About us </span>
+                </v-list-item>
+            </v-toolbar-title>
+
+            <v-toolbar-title   >
+                <v-list-item  :to="'/profile'"> 
+                    <span class=" white--text text--darken-2 border-bottom">  Our specialists </span> 
+                </v-list-item>
+            </v-toolbar-title>
+
+            <v-toolbar-title   >
+                <v-list-item  :to="'/partnerships'"> 
+                    <span class=" white--text text--darken-2">  Partnerships </span>
+                </v-list-item>
+            </v-toolbar-title>
+
+            <v-toolbar-title   >
+                <v-list-item  :to="'/prices'"> 
+                    <span class=" white--text text--darken-2">  Prices </span>
+                </v-list-item>
+            </v-toolbar-title>
+        </v-app-bar>
+
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+    </v-app>
 </template>
 
 <script>
-  import axios from 'axios'
-  export default {
-    data() {
-      return {
-        drawer: null ,
-        items: [
-          { title: 'Personal Details', icon: 'fas fa-search', to: '/personalDetails' },
-          { title: 'History', icon: 'fas fa-list', to: '/appointmentsHistory' },
-          { title: 'Appointment', icon: 'fas fa-edit', to: 'appointment' }
-         
-
-        ],
-        selectedItem: '',
-        patient: {},
-        }
-      },
-
-      computed: {
-       localStorageToken() {
-         
-         if (localStorage && localStorage.token) {
-           return localStorage.token
-         }
-         return null
-         
-       },
-
-       localStorageUser() {
-        
-         if (localStorage && localStorage.user) {
-           return JSON.parse(localStorage.user)
-         }
-          return null
-        
-       }
-      },
-      
-      methods: {
-        getPatientDetails() {
-          if (!this.localStorageUser) {
-            this.$router.push('/login')
-            return
-          
-          }
-
-          let userId = this.localStorageUser.id
-
-          axios.get(`http://localhost:3000/patients/${userId}`).then((response) => {
-                this.patient = response.data.data 
-            })
+    import axios from 'axios'
+    export default {
+        data() {
+            return {
+                drawer: null ,
+                items: [
+                    { title: 'Personal Details', icon: 'fas fa-search', to: '/personalDetails' },
+                    { title: 'History', icon: 'fas fa-list', to: '/appointmentsHistory' },
+                    { title: 'Appointment', icon: 'fas fa-edit', to: 'appointment' }
+                ],
+                selectedItem: '',
+                patient: {},
+            }
         },
 
-        Logout() {
-          this.localStorage.token = null 
-          this.localStorage.user = null
-          this.patient = {}
-          this.$router.push('/login')
+        computed: {
+
+            localStorageToken() {
+                if (localStorage && localStorage.token) {
+                    return localStorage.token
+                }
+                return null
+            },
+
+            localStorageUser() {
+                if (localStorage && localStorage.user) {
+                    return JSON.parse(localStorage.user)
+                }
+                return null
+            }
+        },
+
+        methods: {
+            getPatientDetails() {
+                if (!this.localStorageUser) {
+                this.$router.push('/login')
+                return
+            }
+
+                let userId = this.localStorageUser.id
+
+                axios.get(`http://localhost:3000/patients/${userId}`).then((response) => {
+                    this.patient = response.data.data 
+                })
+            },
+
+            Logout() {
+                this.localStorage.token = null 
+                this.localStorage.user = null
+                this.patient = {}
+                this.$router.push('/login')
+            }
+        },
+
+        async created() {
+            await this.getPatientDetails()
         }
 
-      },
-
-      async created() {
-        await this.getPatientDetails()
-    }
-    
     }
 </script>
 
